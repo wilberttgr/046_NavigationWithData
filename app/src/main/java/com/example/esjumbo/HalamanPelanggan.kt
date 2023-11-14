e4b5b3c8d5733b334412518582754c730578849a
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2023-11-14 17:17:20 +0700

    add var dalam halamanpelanggan

--- app/src/main/java/com/example/esjumbo/HalamanPelanggan.kt
+++ app/src/main/java/com/example/esjumbo/HalamanPelanggan.kt
@@ -1,9 +1,17 @@
 package com.example.esjumbo
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 @Composable
 fun CustomerDetailsScreen(
     onConfirmButtonClicked: (String, String, String) -> Unit,
     onCancelButtonClicked: () -> Unit
-){}
+){
+    var namaPelanggan by remember { mutableStateOf("") }
+    var nomorTelepon by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+}
