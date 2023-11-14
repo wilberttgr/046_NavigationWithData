263052ba37446a197ba84b10611212870752c1e3
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2023-11-14 17:24:34 +0700

    buat Row dan button untuk cancel dan next

--- app/src/main/java/com/example/esjumbo/HalamanPelanggan.kt
+++ app/src/main/java/com/example/esjumbo/HalamanPelanggan.kt
@@ -1,11 +1,15 @@
 package com.example.esjumbo
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -59,5 +63,25 @@ fun CustomerDetailsScreen(
                 .fillMaxWidth()
                 .padding(bottom = 16.dp),
         )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            OutlinedButton(onClick = onCancelButtonClicked) {
+                Text(text = "Cancel")
+            }
+
+            Button(
+                onClick = {
+                    if (namaPelanggan.isNotEmpty() && nomorTelepon.isNotEmpty() && alamat.isNotEmpty()) {
+                        onConfirmButtonClicked(namaPelanggan, nomorTelepon, alamat)
+                    }
+                }
+            ) {
+                Text(text = "Next")
+            }
+        }
     }
 }
