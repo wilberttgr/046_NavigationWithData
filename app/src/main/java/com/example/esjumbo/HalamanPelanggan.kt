a55041352ef3a484a18f4e8e3a0f034eac544438
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2023-11-14 17:19:35 +0700

    membuat column

--- app/src/main/java/com/example/esjumbo/HalamanPelanggan.kt
+++ app/src/main/java/com/example/esjumbo/HalamanPelanggan.kt
@@ -1,10 +1,15 @@
 package com.example.esjumbo
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun CustomerDetailsScreen(
@@ -14,4 +19,10 @@ fun CustomerDetailsScreen(
     var namaPelanggan by remember { mutableStateOf("") }
     var nomorTelepon by remember { mutableStateOf("") }
     var alamat by remember { mutableStateOf("") }
+
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp, vertical = 16.dp)
+    ) {}
 }
