a2c1a494e8e717bf569149d47445df23e1f70d3f
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2023-11-14 17:15:34 +0700

    add fun CustomerDetailsScreen

--- app/src/main/java/com/example/esjumbo/HalamanPelanggan.kt
+++ app/src/main/java/com/example/esjumbo/HalamanPelanggan.kt
@@ -1,2 +1,9 @@
 package com.example.esjumbo
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CustomerDetailsScreen(
+    onConfirmButtonClicked: (String, String, String) -> Unit,
+    onCancelButtonClicked: () -> Unit
+){}
