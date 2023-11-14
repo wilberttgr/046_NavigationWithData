cb9a3f4548677588f9cd8adf3a6861e7f5a8d465
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2023-11-14 17:10:35 +0700

    buat val halamanpelanggan

--- app/src/main/java/com/example/esjumbo/data/OrderUIState.kt
+++ app/src/main/java/com/example/esjumbo/data/OrderUIState.kt
@@ -4,4 +4,7 @@ data class OrderUIState(
     val jumlah: Int = 0,
     val rasa: String = "",
     val harga: String = "",
+    val namaPelanggan: String = "",
+    val nomorTelepon: String = "",
+    val alamat: String = ""
 )
