4b62bd2f0e891e06d26af2f9653f76fa9f7f92ed
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2023-11-14 17:08:23 +0700

    add HalamanPelanggan

--- app/src/main/java/com/example/esjumbo/HalamanDua.kt
+++ app/src/main/java/com/example/esjumbo/HalamanDua.kt
@@ -1,6 +1,6 @@
 package com.example.esjumbo
 import androidx.compose.foundation.layout. Arrangement
-import androidx.compose.foundation.layout.Column import androidx.compose. foundation.layout. Row
+import androidx.compose.foundation.layout.Column import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose. foundation.layout.height
